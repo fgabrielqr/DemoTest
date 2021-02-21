749e4bfeb26f287a88fbb766d72fb17833bc236d
fgabrielqr <56087920+fgabrielqr@users.noreply.github.com>
2021-02-21 14:02:30 -0300

    Add files via upload

---
+++ Demo/src/demo/DemoMainTest.java
@@ -0,0 +1,66 @@
+package demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+
+class DemoMainTest {
+	
+	static private ByteArrayOutputStream baOut;
+	static private PrintStream psOut;
+	
+	@BeforeClass
+	public static void beforeClassInit() {
+		baOut = new ByteArrayOutputStream();
+		psOut = new PrintStream(baOut);
+		System.setOut(psOut);
+	}
+	
+	@AfterClass
+	public static void afterClassFinalize() {
+		psOut.close();
+	}
+	
+	@Before
+	public void setUp() {
+		baOut.reset();
+	}
+
+	@Test
+	public void test_is_triangle_1() {
+		DemoMain.isTriangle(10.0, 14.0, 8.0);
+		String output = baOut.toString();
+		
+		assertEquals("true", output);
+	}
+
+	@Test
+	public void test_is_triangle_2() {
+		DemoMain.isTriangle(1, 5, 3);
+		String output = baOut.toString();
+		
+		assertEquals("true", output);
+	}
+	
+	@Test
+	public void testMain_1() {
+		DemoMain.main(new String [] {""});
+		String output = baOut.toString();
+		
+		assertEquals("This is a triangle.", output);
+	}
+
+	@Test
+	public void testMain_2() {
+		DemoMain.main(new String [] {""});
+		String output = baOut.toString();
+		
+		assertEquals("This is not a triangle.", output);
+	}
+}
