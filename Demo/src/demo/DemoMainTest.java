422d041df88797da3764416456281b1f01eda76c
fgabrielqr <56087920+fgabrielqr@users.noreply.github.com>
2021-02-24 11:00:39 -0300

    Add files via upload

--- Demo/src/demo/DemoMainTest.java
+++ Demo/src/demo/DemoMainTest.java
@@ -6,104 +6,89 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
 class DemoMainTest {
 
-	static private ByteArrayOutputStream baOut;
-	static private PrintStream psOut;
-
-	@BeforeClass
-	public static void beforeClassInit() {
-		baOut = new ByteArrayOutputStream();
-		psOut = new PrintStream(baOut);
-		System.setOut(psOut);
-	}
-
-	@AfterClass
-	public static void afterClassFinalize() {
-		psOut.close();
-	}
-
-	@Before
-	public void setUp() {
-		baOut.reset();
-	}
-
 	@Test
 	public void test_is_triangle_1() {
-		assertTrue(DemoMain.isTriangle(3, 4, 5));
+		assertTrue(DemoMain.isTriangle(2.5, 2.5, 3.8));
 	}
 
 	@Test
 	public void test_is_triangle_2() {
-		assertTrue(DemoMain.isTriangle(5, 12, 13));
+		assertFalse(DemoMain.isTriangle(2.5, 2.5, 0));
 	}
 
 	@Test
 	public void test_is_triangle_3() {
-		assertTrue(DemoMain.isTriangle(5, 13, 12));
+		assertTrue(DemoMain.isTriangle(4, 3, 2));
 	}
 
 	@Test
 	public void test_is_triangle_4() {
-		assertTrue(DemoMain.isTriangle(12, 5, 13));
+		assertFalse(DemoMain.isTriangle(4, 3, 0));
 	}
 
 	@Test
 	public void test_is_triangle_5() {
-		assertTrue(DemoMain.isTriangle(12, 13, 5));
+		assertTrue(DemoMain.isTriangle(4, 4, 4));
 	}
 
 	@Test
 	public void test_is_triangle_6() {
-		assertTrue(DemoMain.isTriangle(0.2, 0.2, 0.2));
+		assertFalse(DemoMain.isTriangle(0, 0, 0));
 	}
 
 	@Test
-	public void test_is_NOT_triangle_1() {
-		assertFalse(DemoMain.isTriangle(5, 7, 13));
+	public void test_is_triangle_7() {
+		assertTrue(DemoMain.isTriangle(28, 21, 35));
 	}
 	@Test
-	public void test_is_NOT_triangle_2 () {
-		assertFalse(DemoMain.isTriangle (5, 13, 7));
+	public void test_is_triangle_8 () {
+		assertFalse(DemoMain.isTriangle (-28, -21, -35));
 	}
 
 	@Test
 	public void test_main_program1() {
-		ByteArrayInputStream in = new ByteArrayInputStream("10\n14\n8\n".getBytes());
+		String input = "5\n12\n13\n";
+		
+		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
 		System.setIn(in);
+		
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
 
 		String[] args = {};
 		DemoMain.main(args);
-		String output = baOut.toString();
 
 		String consoleOutput = "Enter side 1: " + System.lineSeparator();
 		consoleOutput += "Enter side 2: " + System.lineSeparator();
 		consoleOutput += "Enter side 3: " + System.lineSeparator();
 		consoleOutput += "This is a triangle." + System.lineSeparator();
 
-		assertEquals(consoleOutput, output);
+		assertEquals(consoleOutput, out.toString());
 	}
 
 	@Test
 	public void test_main_program2() {
-		ByteArrayInputStream in = new ByteArrayInputStream("1\n5\n3\n".getBytes());
+		String input = "0\n12\n13\n";
+		
+		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
 		System.setIn(in);
+		
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
 
 		String[] args = {};
 		DemoMain.main(args);
-		String output = baOut.toString();
 
 		String consoleOutput = "Enter side 1: " + System.lineSeparator();
 		consoleOutput += "Enter side 2: " + System.lineSeparator();
 		consoleOutput += "Enter side 3: " + System.lineSeparator();
 		consoleOutput += "This is not a triangle." + System.lineSeparator();
 
-		assertEquals(consoleOutput, output);
+		assertEquals(consoleOutput, out.toString());
 	}
 
 }
